e00a603f73b66206eac81f189bb6208a9a247d93
jetci <intelman26@gmail.com>
2025-06-25 12:14:31 +0700

    Update .gitignore and fix Providers.tsx

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -6,21 +6,4 @@ export default function Providers({ children }: { children: ReactNode }) {
   // Wrapper แสดง children โดยตรง
   return <>{children}</>;
 }
-  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return <>{children}</>;
-
-  return (
-    <LoadScriptNext
-      id="gmap-script"
-      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-      language="th"
-    >
-      {children}
-    </LoadScriptNext>
-  );
-}
